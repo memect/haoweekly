ef61fc0f59a0153f83c93c17c4cd9ac5046f8f82
好东西传送门 <hao@memect.com>
2014-11-09 13:27:31 -0800

    minimal index page formatting

--- haoweekly_memect_com/index.php
+++ haoweekly_memect_com/index.php
@@ -1,6 +1,33 @@
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <title>好东西周报 | haoweekly.memect.com | 好东西传送门出品</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</head>
+
+<body style="font-family: Heiti SC,Droidsansfallback,MicrosoftYaHei,Helvetica,Arial,sans-serif; margin-left: auto; margin-right: auto; width: 95%; max-width: 800px;">
 <h2>好东西周报</h2> 
 
 好东西传送门每周推送总结
 
 （待发布）
 
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//anna.memect.co/piwik/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 1]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//anna.memect.co/piwik/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
+</body>
+</html>
